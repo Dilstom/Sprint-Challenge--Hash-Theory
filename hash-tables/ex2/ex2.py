867674798377b215625460e022839609f1b91d1c
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-10-19 11:25:02 -0700

    ex2

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,6 +1,26 @@
 def reconstruct_trip(tickets):
-  pass 
+    dic={}
+    route = ['']*(len(tickets)-1)
+
+    for i in tickets:
+        dic[i[0]] = i[1]  # dic{ start: destination,  }
+        if(i[0] == None):
+            route[0] = i[1]
+    for i in range(1, len(route)):
+        if(route[i-1]) in dic:
+            route[i]=dic[route[i-1]]
+        else:
+            return[]
+    return route
+    
+
+ 
 
 if __name__ == '__main__':
-  # You can write code here to test your implementation using the Python repl
-  pass
+      # You can write code here to test your implementation using the Python repl
+    short_set = [
+      (None, 'PDX'),
+      ('PDX', 'DCA'),
+      ('DCA', None)
+    ]
+    assertEqual(reconstruct_trip(short_set), ['PDX', 'DCA'])
