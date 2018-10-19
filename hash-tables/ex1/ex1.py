93c1f564daff68ed293dff67c1f9f1295f524a62
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-10-19 08:40:53 -0700

    ex1

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,6 +1,12 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+    dic={}
+    for i in range(len(weights)):
+        if(limit-weights[i]) in dic:
+            return(i, dic[limit-weights[i]])
+        else:
+            dic[weights[i]]=i;
+    
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
-  pass 
+  print(get_indices_of_item_weights([4,6,10,15,6], 21))
